2758bdefe5ccb78798c4fea145df3a3e21ea2a13
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2020-08-07 04:02:57 +0700

    first commit for added lara to repo

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->binary('photo')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
