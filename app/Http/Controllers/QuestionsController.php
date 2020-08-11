94402cd3ef4ec0082d9ecaebfef0caec2989b86c
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2020-08-12 00:02:05 +0700

    added comments

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB; digunakan untuk Query Builder
 use App\Question;
 
 class QuestionsController extends Controller
 {
+    //Select ALl Data to Homepage
     public function index()
     {
+        //Query Builder
         // $data = DB::table('questions')->get();
 
+        //ORM
         $data = Question::all();
 
         return view('index', ['data' => $data]);
@@ -22,6 +25,7 @@ class QuestionsController extends Controller
         return view('form');
     }
 
+    //Insert New Data
     public function store(Request $req)
     {
         $req->validate([
@@ -29,11 +33,13 @@ class QuestionsController extends Controller
             "isi" => 'required|max:255'
         ]);
 
+        //Query Builder
         // DB::table('questions')->insert([
         //     "judul" => $req["judul"],
         //     "isi" => $req["isi"]
         // ]);
 
+        //ORM
         $data = new Question;
 
         $data->judul = $req->judul;
@@ -44,23 +50,31 @@ class QuestionsController extends Controller
         return redirect('/quest');
     }
 
+    //For detail page
     public function show($id)
     {
+        //Query Builder
         // $data = DB::table('questions')->where('id', $id)->first();
 
+        // ORM
         $data = Question::where('id', $id)->first();
 
         return view('detail', ['data' => $data]);
     }
 
+    //Select Data to Edit Form
     public function edit($id)
     {
+        //Query Builder
         // $data = DB::table('questions')->where('id', $id)->first();
+
+        //ORM
         $data = Question::where('id', $id)->first();
 
         return view('edit', ['data' => $data]);
     }
 
+    //Update Data
     public function update($id, Request $req)
     {
         $req->validate([
@@ -68,6 +82,7 @@ class QuestionsController extends Controller
             "isi" => 'required|max:255'
         ]);
 
+        //Query Builder
         // DB::table('questions')
         //     ->where('id', $id)
         //     ->update([
@@ -75,6 +90,7 @@ class QuestionsController extends Controller
         //         "isi"   => $req["isi"]
         //     ]);
 
+        //ORM
         $data = Question::find($id);
 
         $data->judul = $req->judul;
@@ -85,10 +101,13 @@ class QuestionsController extends Controller
         return redirect('/quest');
     }
 
+    //Delete Data
     public function destroy($id)
     {
+        //Query Builder
         // DB::table('questions')->where('id', $id)->delete();
 
+        //ORM
         $data = Question::find($id);
 
         $data->delete();
