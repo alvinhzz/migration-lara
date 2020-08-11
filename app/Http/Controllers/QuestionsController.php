6562cfdbbdec660fef3b463cbaa8085f5bde3632
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2020-08-11 23:55:44 +0700

    mengganti query builder dengan Eloquent ORM

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Question;
 
 class QuestionsController extends Controller
 {
     public function index()
     {
-        $data = DB::table('questions')->get();
+        // $data = DB::table('questions')->get();
+
+        $data = Question::all();
+
         return view('index', ['data' => $data]);
     }
 
@@ -25,24 +29,34 @@ class QuestionsController extends Controller
             "isi" => 'required|max:255'
         ]);
 
-        DB::table('questions')->insert([
-            "judul" => $req["judul"],
-            "isi" => $req["isi"]
-        ]);
+        // DB::table('questions')->insert([
+        //     "judul" => $req["judul"],
+        //     "isi" => $req["isi"]
+        // ]);
+
+        $data = new Question;
+
+        $data->judul = $req->judul;
+        $data->isi = $req->isi;
+
+        $data->save();
 
         return redirect('/quest');
     }
 
     public function show($id)
     {
-        $data = DB::table('questions')->where('id', $id)->first();
+        // $data = DB::table('questions')->where('id', $id)->first();
+
+        $data = Question::where('id', $id)->first();
 
         return view('detail', ['data' => $data]);
     }
 
     public function edit($id)
     {
-        $data = DB::table('questions')->where('id', $id)->first();
+        // $data = DB::table('questions')->where('id', $id)->first();
+        $data = Question::where('id', $id)->first();
 
         return view('edit', ['data' => $data]);
     }
@@ -50,23 +64,34 @@ class QuestionsController extends Controller
     public function update($id, Request $req)
     {
         $req->validate([
-            "judul" => 'required|max:55|unique:questions',
+            "judul" => 'required|max:55',
             "isi" => 'required|max:255'
         ]);
 
-        DB::table('questions')
-            ->where('id', $id)
-            ->update([
-                "judul" => $req["judul"],
-                "isi"   => $req["isi"]
-            ]);
+        // DB::table('questions')
+        //     ->where('id', $id)
+        //     ->update([
+        //         "judul" => $req["judul"],
+        //         "isi"   => $req["isi"]
+        //     ]);
+
+        $data = Question::find($id);
+
+        $data->judul = $req->judul;
+        $data->isi = $req->isi;
+
+        $data->save();
 
         return redirect('/quest');
     }
 
     public function destroy($id)
     {
-        DB::table('questions')->where('id', $id)->delete();
+        // DB::table('questions')->where('id', $id)->delete();
+
+        $data = Question::find($id);
+
+        $data->delete();
 
         return redirect('/quest');
     }
---
+++ app/Question.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+}
